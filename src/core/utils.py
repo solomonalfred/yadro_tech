b97e146906362b26f0ff9327f8e3bb53d5280cbe
0riginaln0 <gooddmitriy@mail.ru>
2025-05-25 05:22:17 +0300

    docker fix

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+
 from src.shemas import Node, Edge
 
 
--- src/models/graph.py
+++ src/models/graph.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from typing import List
-
 from sqlalchemy import (
     DateTime,
     ForeignKey,
